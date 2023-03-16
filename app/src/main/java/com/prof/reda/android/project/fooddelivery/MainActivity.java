31dbd4ca682a542f7722cc5d496859247b939a9b
redaxxxx <redanabil518@gmail.com>
2023-03-17 01:22:46 +0200

    Master

---
+++ app/src/main/java/com/prof/reda/android/project/fooddelivery/MainActivity.java
@@ -0,0 +1,14 @@
+package com.prof.reda.android.project.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
