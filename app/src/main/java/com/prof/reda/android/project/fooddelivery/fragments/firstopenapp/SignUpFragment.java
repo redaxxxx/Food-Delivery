b001de8d3c788e48381abf7626af63e00e526fa6
redaxxxx <redanabil518@gmail.com>
2023-03-17 23:36:08 +0200

    Master

--- app/src/main/java/com/prof/reda/android/project/fooddelivery/fragments/firstopenapp/SignUpFragment.java
+++ app/src/main/java/com/prof/reda/android/project/fooddelivery/fragments/firstopenapp/SignUpFragment.java
@@ -2,20 +2,37 @@ package com.prof.reda.android.project.fooddelivery.fragments.firstopenapp;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.prof.reda.android.project.fooddelivery.R;
+import com.prof.reda.android.project.fooddelivery.databinding.FragmentSignUpBinding;
 
 public class SignUpFragment extends Fragment {
 
+    private FragmentSignUpBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up, container, false);
+
+        binding.alreadyHaveAccount.setOnClickListener(view -> {
+            Navigation.findNavController(view).navigate(R.id.action_signUpFragment_to_signInFragment);
+        });
+
+        binding.createBtn.setOnClickListener(view -> {
+            createAccountWithFirebase();
+        });
+        return binding.getRoot();
+    }
+
+    private void createAccountWithFirebase() {
+
     }
 }
