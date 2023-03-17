6212ca5df92729ef8b457be536dee094e7a655d7
redaxxxx <redanabil518@gmail.com>
2023-03-17 07:26:12 +0200

    Master

--- app/src/main/java/com/prof/reda/android/project/fooddelivery/MainActivity.java
+++ app/src/main/java/com/prof/reda/android/project/fooddelivery/MainActivity.java
@@ -1,14 +1,55 @@
 package com.prof.reda.android.project.fooddelivery;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.prof.reda.android.project.fooddelivery.databinding.ActivityMainBinding;
+import com.prof.reda.android.project.fooddelivery.fragments.SecondOnbaordingFragment;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.NavHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.NavHostFragment);
+        return navController.navigateUp();
+    }
+
+    public void onResume() {
+        super.onResume();
+        ActionBar supportActionBar = ((AppCompatActivity) this).getSupportActionBar();
+        if (supportActionBar != null)
+            supportActionBar.hide();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ActionBar supportActionBar = ((AppCompatActivity) this).getSupportActionBar();
+        if (supportActionBar != null)
+            supportActionBar.show();
     }
 }
--- app/src/main/java/com/prof/reda/android/project/fooddelivery/fragments/OnbaordingFragment.java
+++ app/src/main/java/com/prof/reda/android/project/fooddelivery/fragments/OnbaordingFragment.java
@@ -1,66 +1,75 @@
 package com.prof.reda.android.project.fooddelivery.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.prof.reda.android.project.fooddelivery.R;
+import com.prof.reda.android.project.fooddelivery.databinding.FragmentOnbaordingBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OnbaordingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class OnbaordingFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OnbaordingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OnbaordingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OnbaordingFragment newInstance(String param1, String param2) {
-        OnbaordingFragment fragment = new OnbaordingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FragmentOnbaordingBinding binding;
+    private Timer timer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onbaording, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_onbaording, container, false);
+
+
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        timer = new Timer();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Navigation.findNavController(view).navigate(R.id.action_onbaordingFragment_to_secondOnbaordingFragment);
+            }
+        }, 5000);
+
+
+    }
+
+
+    //    private void isFirstTime() {
+//        //for checking if the app is running for the very first time
+//        //we need to saved a value to shared preferences
+//        SharedPreferences preferences = getActivity().getSharedPreferences("onBoard", Context.MODE_PRIVATE);
+//        boolean isFirstTime = preferences.getBoolean("isFirstTime", true);
+//
+//        //default value
+//        if (isFirstTime){
+//            //if its true then its first time and we will change it false
+//            SharedPreferences.Editor editor = preferences.edit();
+//            editor.putBoolean("isFirstTime", false);
+//            editor.apply();
+//
+//            Navigation.createNavigateOnClickListener(R.id.action_onbaordingFragment_to_secondOnbaordingFragment);
+//        }else {
+//            //Start Home
+//        }
+//    }
 }
